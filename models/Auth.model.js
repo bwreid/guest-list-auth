a2a2b23cedd2fd2d285b8e2a48a8cf8efe64aab6
Wes Reid <bwreid@gmail.com>
2017-11-27 20:36:41 -0800

    Minor refactors to User and Auth models

--- models/Auth.model.js
+++ models/Auth.model.js
@@ -1,5 +1,5 @@
-const { verify } = require('jsonwebtoken')
 const { promisify } = require('util')
+const { verify } = require('jsonwebtoken')
 const verifyAsync = promisify(verify)
 
 class Auth {
@@ -10,31 +10,25 @@ class Auth {
 
   static isUser (id, token) {
     return Auth.isAuthenticated(token).then(payload => {
-      if (payload.sub.id == id) {
-        return payload
-      } else {
-        throw new Error()
-      }
+      if (payload.sub.id != id) throw new Error(`You are not authorized to access that route`)
+
+      return payload
     })
   }
 
   static isVip (token) {
     return Auth.isAuthenticated(token).then(payload => {
-      if (payload.sub.role !== 'user') {
-        return payload
-      } else {
-        throw new Error()
-      }
+      if (payload.sub.role === 'user') throw new Error(`You are not authorized to access that route`)
+
+      return payload
     })
   }
 
   static isAdmin (id, token) {
     return Auth.isAuthenticated(token).then(payload => {
-      if (payload.sub.role === 'admin') {
-        return payload
-      } else {
-        throw new Error()
-      }
+      if (payload.sub.role !== 'admin') throw new Error(`You are not authorized to access that route`)
+
+      return payload
     })
   }
 }
--- models/User.model.js
+++ models/User.model.js
@@ -2,34 +2,30 @@ const { TABLE_NAMES } = require('../constants')
 const db = require('../db')
 const Model = require('./Model')(TABLE_NAMES.USER)
 
-const { sign } = require('jsonwebtoken')
 const { promisify } = require('util')
+const { sign } = require('jsonwebtoken')
 const signAsync = promisify(sign)
 
 class User extends Model {
   static login (email, password) {
     return db('users').where({ email }).first().then(user => {
-      if (user.password === password) {
-        const { id, role } = user
-        const sub = { id, role }
-        const expiresIn = '30 days'
-        const secret = process.env.SECRET_KEY
+      if (user.password !== password) throw new Error(`User authentication failed`)
 
-        return signAsync({ sub }, secret, { expiresIn })
-      } else {
-        throw new Error(`User authentication failed`)
-      }
-    })
+      const { id, role } = user
+      const sub = { id, role }
+      const secret = process.env.SECRET_KEY
+      const expiresIn = '30 days'
+
+      return signAsync({ sub }, secret, { expiresIn })
+    }).catch(error => { throw new Error(error) })
   }
 
   static signup (email, password) {
     return db('users').where({ email }).first().then(user => {
-      if (user) {
-        throw new Error(`Email has already been taken`)
-      } else {
-        return User.create({ email, password })
-      }
-    }).catch(() => { throw new Error() })
+      if (user) throw new Error(`Email has already been taken`)
+
+      return User.create({ email, password })
+    }).catch(error => { throw new Error(error) })
   }
 }
 
